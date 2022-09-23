0b2cda6a92406a8b445b354d98bd6438301bdb67
하정원 <sch20215104@gmail.com>
2022-09-23 21:56:26 +0900

    초기 커밋.

---
+++ Cpp week 04/4-1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int countDigit(int n) {
+    if (n == 0)
+        return 1;
+    int count = 0;
+    while (n != 0) {
+        n = n / 10;
+        ++count;
+    }
+    return count;
+}
+
+void countDigit(string c) {
+    countDigit(c);
+}
+
+int main() {
+    cout << "숫자 12345의 자릿수는? : " << countDigit(12345) << endl;
+    cout << "문자열 12345의 자릿수는? : " << countDigit(12345) << endl;
+}
